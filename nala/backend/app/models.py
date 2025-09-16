6c38efaf542d4f2ae04cf11a1678a39cd4dfe30d
yponvon <yvonnewenqian29@gmail.com>
2025-09-17 01:51:05 +0800

    course and student models

--- nala/backend/app/models.py
+++ nala/backend/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# ThreadMap
 class Node(models.Model):
     name = models.CharField(max_length=255, unique=True)
     summary = models.TextField(max_length=500, unique=True)
@@ -31,4 +32,22 @@ class Concept(Node):
     
     def __str__(self):
         return f'Concept: {self.name} of topic "{self.topic.name}" '
+
+# Course and Student
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Course: {self.title}'
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    enrolled_courses = models.ManyToManyField(Course, related_name='students', blank=True)
+
+    def __str__(self):
+        return f'Student: {self.name}'
     
+

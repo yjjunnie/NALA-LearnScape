56c12c11c75469ab075942a10b7026f8fd73d04a
yjjunnie <161452557+yjjunnie@users.noreply.github.com>
2025-09-28 20:13:53 +0800

    Update App.tsx

--- nala/frontend/nalaLearnscape/src/App.tsx
+++ nala/frontend/nalaLearnscape/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import ThreadMap_try from "./pages/ThreadMap_try";
+import ThreadMap_try from "./pages/ThreadMap";
 import "./App.css";
 
 function App() {

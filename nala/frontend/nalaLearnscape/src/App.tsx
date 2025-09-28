9e8836469d7351bc70a932d448e7ce80f9ae84e3
Michelle <michlaye@gmail.com>
2025-09-28 21:26:11 +0800

    navigation

--- nala/frontend/nalaLearnscape/src/App.tsx
+++ nala/frontend/nalaLearnscape/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import ThreadMap_try from "./pages/ThreadMap";
 import "./App.css";
+import { Modules } from "./pages/Modules";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Modules" element={<Modules />} />
         <Route path="/threadmap" element={<ThreadMap_try />} />
       </Routes>
     </Router>

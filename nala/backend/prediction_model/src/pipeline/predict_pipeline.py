4e35522966860116e33aba8bc7ee377049c011b2
yponvon <yvonnewenqian29@gmail.com>
2025-10-01 05:31:14 +0800

    scheduler

--- nala/backend/prediction_model/src/pipeline/predict_pipeline.py
+++ nala/backend/prediction_model/src/pipeline/predict_pipeline.py
@@ -7,14 +7,17 @@ from src.utils import load_object
 
 class PredictPipeline:
     def __init__(self):
-        pass
+        # Get the absolute path to the current file’s directory
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        # Build the path to the artifacts folder dynamically
+        self.artifacts_dir = os.path.join(self.current_dir, "artifacts")
 
     def predict(self,features):
         try:
             print("here")
             print("Before Loading")
-            model_path = "/Users/School/Desktop/Repos/NALA-LearnScape/nala/backend/prediction_model/artifacts/model.pkl"
-            preprocessor_path = "/Users/School/Desktop/Repos/NALA-LearnScape/nala/backend/prediction_model/artifacts/preprocessor.pkl"
+            model_path = os.path.join(self.artifacts_dir, "model.pkl")
+            preprocessor_path = os.path.join(self.artifacts_dir, "preprocessor.pkl")
             model=load_object(model_path)
             preprocessor=load_object(preprocessor_path)
             print("After Loading")

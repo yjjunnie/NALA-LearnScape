28b0d51b15a0f7488d1200afb19db264f779c0a1
Ariq Koh <105147577+sellmenn@users.noreply.github.com>
2025-10-01 00:47:18 +0800

    Add Bloom-aware threadmap data and chatbot scenarios

--- nala/frontend/nalaLearnscape/src/App.tsx
+++ nala/frontend/nalaLearnscape/src/App.tsx
@@ -6,6 +6,7 @@ import { Modules } from "./pages/Modules";
 import ModuleInfo from "./pages/ModuleInfo";
 import KnowledgeCapsule from "./pages/KnowledgeCapsule";
 import Quiz from "./pages/Quiz";
+import Chatbot from "./pages/Chatbot";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/threadmap" element={<ThreadMap />} />
         <Route path="/Modules/:moduleId/Topics/:topicId/Notes" element={<KnowledgeCapsule />} />
         <Route path="/Modules/:moduleId/quiz" element={<Quiz />} />
+        <Route path="/Modules/:moduleId/chatbot" element={<Chatbot />} />
       </Routes>
     </Router>
   );
